149d13c4e6ad6553f80a571fc00bc748ca4fee81
Kpeters <kaitlynpeters@cox.net>
2023-12-30 12:40:01 -0600

    animation

--- utils/animation.py
+++ utils/animation.py
@@ -1,7 +1,6 @@
 import pygame
 
 class Animations:
-   
     def __init__(self, images, idleSprite=None, deltaTime=6):
         self.images = images
         self.index = 0 #index of current image of animation
